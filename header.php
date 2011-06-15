c9267f0c8486940fe3dffd3cc553210fafc2a1fa
Chris Reynolds <chris@jazzsequence.com>
2011-06-14 23:08:34 -0600

    uncommented the dynamic title tags in the header (will change these around later) changed path to reset styles removed link to non-existant fonts.css (but may add another one later)

--- header.php
+++ header.php
@@ -2,21 +2,20 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-type" content="text/html;charset=utf-8" />	
-	<title><?php wp_title(''); /*
+	<title><?php
 	$category = get_the_category();
 	if (is_home () ) { bloginfo('name'); }
 	elseif ( is_category() ) { single_cat_title(); echo ' | ' ; bloginfo('name'); }
 	elseif (is_single() ) { single_post_title(); echo ' | '; echo $category[0]->cat_name; }
 	elseif (is_page() ) { single_post_title();}
-	else { wp_title('',true); } ?> | <?php bloginfo('description'); */ ?></title>
+	else { wp_title('',true); } ?> | <?php bloginfo('description'); ?></title>
 	<link rel="Shortcut Icon" href="<?php bloginfo('template_url'); ?>/images/favicon.ico" type="image/x-icon" />
-    <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/blueprint/screen.css" type="text/css" media="screen, projection" />
-    <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/blueprint/print.css" type="text/css" media="print" /> 
+    <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/css/screen.css" type="text/css" media="screen, projection" />
+    <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/css/print.css" type="text/css" media="print" /> 
     <!--[if lt IE 8]>
-      <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/blueprint/ie.css" type="text/css" media="screen, projection" />
+      <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/css/ie.css" type="text/css" media="screen, projection" />
     <![endif]-->	
 	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
-    <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/fonts/fonts.css" type="text/css" media="screen, projection" />    
 	<link rel="alternate" type="application/rss+xml" title="RSS 2.0" href="<?php bloginfo('rss2_url'); ?>" />
 	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
   	<script language="javascript" src="<?php bloginfo('template_url'); ?>/js/suckerfish.js" type="application/javascript"></script>
