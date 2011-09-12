214d40603f58a72ac52cb5c5ff0b644c9fc5aa82
chris reynolds <chris@jazzsequence.com>
2011-09-12 10:33:51 -0600

    changed footer text

--- footer.php
+++ footer.php
@@ -15,11 +15,11 @@
         <div class="rightbox span-7 last">
 		 <ul>
 			 <?php if( !function_exists('dynamic_sidebar') || !dynamic_sidebar('Right Footer Box') ) : ?>
-             <?php endif; }?>                
+             <?php endif; ?>                
           </ul>      
         </div>
         <div class="spacer-10"></div>
-		<div class="credit">&copy; <?php echo date('Y'); ?> <?php bloginfo('title'); ?> | <a href="http://museumthemes.com/blog/free-themes/ap-blueprint/" title="AP-Blueprint | A free WordPress theme framework" target="_blank">AP-Blueprint</a> is a <a href="http://museumthemes.com/category/free-themes/" target="_blank" title="Museum Themes | Fine Art WordPress Themes">free WordPress theme</a> by <a href="http://www.arcanepalette.com/" title="arcane palette creative design | artistic website design" target="_blank">Arcane Palette</a></div>
+		<div class="credit">&copy; <?php echo date('Y'); ?> <?php bloginfo('title'); ?> | Core is a <a href="http://wordpress.org" target="_blank">WordPress</a> theme framework by <a href="http://museumthemes.com/category/free-themes/" target="_blank" title="Museum Themes | Fine Art WordPress Themes">Museum Themes</a></div>
         </div>
 </div>
 
