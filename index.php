c6b67367b121721ba9ce83b5760d321b5e25fe9e
chris reynolds <chris@jazzsequence.com>
2011-09-22 20:53:34 -0600

    fixed the bugs (can't get_template_part and then get_template_part again, apparently...)

--- index.php
+++ index.php
@@ -6,7 +6,17 @@
 <?php get_header(); ?>
 <div class="content">
 
-	<?php get_template_part('post', get_post_format()); ?>
+	<?php if (have_posts()) : while (have_posts()) : the_post(); 
+		$post_format = get_post_format();
+		get_template_part('post',$post_format);
+		endwhile; ?>
+	<nav class="navigation">
+		<?php  if(function_exists('wp_pagenavi')) { wp_pagenavi(); } else { ?>
+		<div class="alignleft"><?php next_posts_link('&laquo; Older Entries') ?></div>
+		<div class="alignright"><?php previous_posts_link('Newer Entries &raquo;') ?></div>
+		<?php } ?>
+	</nav>
+	<?php endif; ?>
 
 </div>
 <?php get_sidebar(); ?>
--- post-aside.php
+++ post-aside.php
@@ -1,8 +1,3 @@
-<?php if (have_posts()) : ?>
-<?php while (have_posts()) : the_post(); ?>
-<?php /*
-	This theme supports post formats.  In the future, I'll probably need to add some kind of conditional that fetches the post format of the current post and outputs a different HTML5 tag appropriate to that post format (e.g. <audio>, <video>, <aside>, etc).  For now, we're just going to spit out <article>
-*/ ?>
 	<article <?php post_class(); ?> id="post-<?php the_ID(); ?>">				
 		<h3 class="the_date"><time datetime=<?php the_time('Y-m-d'); ?>><?php the_time('j F Y') ?></time></h3>
 		<h2 class="the_title"><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
@@ -16,13 +11,3 @@
          </section>
 	</article>
     <div class="clear"></div>
-
-	<?php endwhile; ?>
-
-	<nav class="navigation">
-		<?php  if(function_exists('wp_pagenavi')) { wp_pagenavi(); } else { ?>
-		<div class="alignleft"><?php next_posts_link('&laquo; Older Entries') ?></div>
-		<div class="alignright"><?php previous_posts_link('Newer Entries &raquo;') ?></div>
-		<?php } ?>
-	</nav>
-	<?php endif; ?>
