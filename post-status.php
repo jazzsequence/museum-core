5c6cb9dcecbf805d94ee9a1d3c6e457b298dd029
chris reynolds <chris@jazzsequence.com>
2012-03-15 21:35:57 -0600

    added a clear before the wp_link_pages

--- post-status.php
+++ post-status.php
@@ -1,7 +1,8 @@
-	<article <?php post_class(); ?> id="post-<?php the_ID(); ?>">				
+	<article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
 		<section class="entry">
 			<?php the_content('Read more &raquo;'); ?>
-			<?php wp_link_pages(); ?>			
+			<div class="clear"></div>
+			<?php wp_link_pages(); ?>
 		</section>
 		<section class="postmetadata">
 			<time datetime=<?php the_time('Y-m-d'); ?>><?php echo human_time_diff( get_the_time('U'), current_time('timestamp') ) . ' ago'; ?></time>
