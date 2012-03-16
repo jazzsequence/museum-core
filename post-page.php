b14d55b1d577d046473c8ac5db388d90089b6d9a
chris reynolds <chris@jazzsequence.com>
2012-03-15 21:41:10 -0600

    added a clear above the wp_link_pages

--- post-page.php
+++ post-page.php
@@ -1,16 +1,17 @@
 	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
- 		<article <?php post_class(); ?> id="post-<?php the_ID(); ?>">				
+ 		<article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
 			<time datetime=<?php the_time('Y-m-d'); ?>></time>
 			<h2 class="the_title"><?php the_title(); ?></h2>
             <div class="clear"></div>
 			<section class="entry">
 				<?php the_content('Read more &raquo;'); ?>
-				<?php wp_link_pages(); ?>					
+				<div class="clear"></div>
+				<?php wp_link_pages(); ?>
 			</section>
 			<section class="postmeta">
 				<p><?php edit_post_link('Edit this entry','','.'); ?></p>
 			</section>
     	<div class="clear"></div>
 		</article>
-		
+
 	<?php endwhile; endif; ?>
