2cb259e22180537958f23a0967f11eaa17e928ef
chris reynolds <chris@jazzsequence.com>
2012-03-15 21:35:35 -0600

    added post thumbnail support and i18nized search results text

--- post-search.php
+++ post-search.php
@@ -2,30 +2,33 @@
     global $query_string;
     $query_args = explode("&", $query_string);
     $search_query = array();
-   
+
     foreach($query_args as $key => $string) {
         $query_split = explode("=", $string);
         $search_query[$query_split[0]] = $query_split[1];
     } // foreach
-    
+
     $search = new WP_Query($search_query);
- 
+
     global $wp_query;
     $total_results = $wp_query->found_posts;
 ?>
-	<h2 class="searchresults">We found <?php echo $total_results; ?> results for &ldquo;<?php echo get_search_query(); ?>&rdquo;</h2>
-	
+	<h2 class="searchresults the_title"><?php _e( 'We found ', 'ap_core' ); echo $total_results; _e( ' results for ', 'ap_core' ); ?>&ldquo;<?php echo get_search_query(); ?>&rdquo;</h2>
+
 	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-	<article <?php post_class(); ?> id="post-<?php the_ID(); ?>">				
+	<article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
 		<h3 class="the_date"><time datetime=<?php the_time('Y-m-d'); ?>><?php the_time(get_option('date_format')) ?></time></h3>
 		<h2 class="the_title"><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
         <div class="clear"></div>
 		<section class="entry">
+			<?php if(has_post_thumbnail()) { ?>
+				<div class="alignleft twocol"><a href="<?php the_permalink() ?>" title="<?php the_title_attribute(); ?>"><?php the_post_thumbnail(); ?></a></div>
+			<?php } ?>
 			<?php the_excerpt(); ?>
 		</section>
 		<section class="postmetadata">
-            Posted in <?php the_category(',&nbsp;'); ?> <?php the_tags('| Tags: ',', ',''); ?><br />
-            <?php comments_popup_link('No Comments &#187;', 'One Comment &#187;', '% Comments &#187;'); ?>       
+            Posted in <?php the_category(', '); ?> <?php the_tags('and tagged ',', ',''); ?><br />
+            <?php comments_popup_link('No Comments &#187;', 'One Comment &#187;', '% Comments &#187;'); ?>
         </section>
 	</article>
     <div class="clear"></div>
