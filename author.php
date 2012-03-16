c1da80af30cedd9f3b889f51b748911ae813076f
chris reynolds <chris@jazzsequence.com>
2012-03-15 21:04:07 -0600

    custom author template

--- author.php
+++ author.php
@@ -6,7 +6,30 @@ get_header();
 include( AP_CORE_OPTIONS ); ?>
 <div class="content ninecol<?php echo $right; ?>">
 
-	<?php get_template_part('post', 'author'); ?>
+	<?php if ( have_posts() ) the_post();
+		$description = get_the_author_meta('description');
+		$url = get_the_author_meta('user_url');
+	?>
+	<section class="author">
+		<div class="alignleft twocol"><?php echo get_avatar( get_the_author_meta('ID'), $size = '96' ); ?></div>
+		<div class="tencol last">
+			<h2 class="the_title"><?php the_author_meta('display_name') ?></h2>
+			<p>
+				<?php if (isset($description)) {
+					echo $description; ?><br />
+				<?php }
+				if (isset($url)) { ?>
+					<a href="<?php echo $url; ?>" rel="me">Website</a>
+				<?php } ?>
+			</p>
+		</div>
+		<h3 class="alt"><?php _e( 'All posts by ', 'ap_core' ); the_author_meta('display_name'); ?></h3>
+	</section>
+	<div class="clear"></div>
+	<?php
+		rewind_posts();
+		get_template_part('post', 'author');
+	?>
 
 </div>
 <?php get_sidebar(); ?>
--- post-author.php
+++ post-author.php
@@ -1,18 +1,13 @@
-<?php if (have_posts()) : ?>
-<?php while (have_posts()) : the_post(); ?>
-<?php /*
-	This theme supports post formats.  In the future, I'll probably need to add some kind of conditional that fetches the post format of the current post and outputs a different HTML5 tag appropriate to that post format (e.g. <audio>, <video>, <aside>, etc).  For now, we're just going to spit out <article>
-*/ ?>
-	<article <?php post_class(); ?> id="post-<?php the_ID(); ?>">				
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+	<article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
 		<h3 class="the_date"><time datetime=<?php the_time('Y-m-d'); ?>><?php the_time(get_option('date_format')) ?></time></h3>
 		<h2 class="the_title"><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
         <div class="clear"></div>
 		<section class="entry">
-			<?php the_content('Read more &raquo;'); ?>			
-		</section>
+			<?php the_content('Read more &raquo;'); ?>
 		<section class="postmetadata">
             Posted in <?php the_category(',&nbsp;'); ?> <?php the_tags('| Tags: ',', ',''); ?><br />
-            <?php comments_popup_link('No Comments &#187;', 'One Comment &#187;', '% Comments &#187;'); ?>       
+            <?php comments_popup_link('No Comments &#187;', 'One Comment &#187;', '% Comments &#187;'); ?>
          </section>
 	</article>
     <div class="clear"></div>
