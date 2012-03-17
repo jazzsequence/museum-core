d08cfebb5015cfbdc0077ea4bf513450f7d7afe6
chris reynolds <chris@jazzsequence.com>
2012-03-17 09:02:31 -0600

    updated footer to use 1140 grid classes

--- footer.php
+++ footer.php
@@ -1,25 +1,25 @@
-		<footer>
-			<div class="leftbox">
+		<footer class="row">
+			<div class="fourcol" id="leftbox">
 				<ul>
 					<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('Left Footer Box') ) : ?>
-					<?php endif; ?>        
-				</ul>         
-			</div>
-			<div class="middlebox">
-				<ul>
-					 <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('Center Footer Box') ) : ?>
-					 <?php endif; ?>                
+					<?php endif; ?>
 				</ul>
 			</div>
-			<div class="rightbox">
+			<div class="fourcol" id="middlebox">
+				<ul>
+					 <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('Center Footer Box') ) : ?>
+					 <?php endif; ?>
+				</ul>
+			</div>
+			<div class="fourcol last" id="rightbox">
 				<ul>
 					 <?php if( !function_exists('dynamic_sidebar') || !dynamic_sidebar('Right Footer Box') ) : ?>
-					 <?php endif; ?>                
-				</ul>      
+					 <?php endif; ?>
+				</ul>
 			</div>
 			<div class="spacer-10"></div>
 			<?php wp_nav_menu( array( 'container' => 'nav', 'container_class' => 'footernav', 'theme_location' => 'footer', 'fallback_cb' => false ) ); ?>
-			<div class="credit">&copy; <?php echo date('Y'); ?> <?php bloginfo('title'); ?> | Core is a <a href="http://wordpress.org" target="_blank">WordPress</a> theme framework by <a href="http://museumthemes.com/category/free-themes/" target="_blank" title="Museum Themes | Fine Art WordPress Themes">Museum Themes</a></div>			
+			<div class="credit">&copy; <?php echo date('Y'); ?> <?php bloginfo('title'); ?> | Core is a <a href="http://wordpress.org" target="_blank">WordPress</a> theme framework by <a href="http://museumthemes.com/category/free-themes/" target="_blank" title="Museum Themes | Fine Art WordPress Themes">Museum Themes</a></div>
 		</footer>
 	</div><!-- closes .container -->
 
