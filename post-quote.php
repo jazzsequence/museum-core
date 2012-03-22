91a548b5f4ba81786a7c2babfd215449a50010b1
chris reynolds <chris@jazzsequence.com>
2012-03-22 16:49:47 -0600

    added a human-time-diff class to quote and status posts

--- post-quote.php
+++ post-quote.php
@@ -5,7 +5,7 @@
 			<?php wp_link_pages(); ?>
 		</section>
 		<section class="postmetadata">
-			<time datetime=<?php the_time('Y-m-d'); ?>><?php echo human_time_diff( get_the_time('U'), current_time('timestamp') ) . __(' ago','museum-core'); ?></time>
+			<time class="human-time-diff" datetime=<?php the_time('Y-m-d'); ?>><?php echo human_time_diff( get_the_time('U'), current_time('timestamp') ) . __(' ago','museum-core'); ?></time>
          </section>
 	</article>
     <div class="clear"></div>
