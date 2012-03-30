c9dd302ced2fcc990d87391cdb87418057ac734b
chris reynolds <chris@jazzsequence.com>
2012-03-29 21:26:40 -0600

    fixed (for real this time) internationalization

--- post-chat.php
+++ post-chat.php
@@ -1,6 +1,6 @@
 	<article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
 		<time datetime=<?php the_time('Y-m-d'); ?>></time>
-		<h2 class="the_title"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php _e('Permanent Link to','museum-core'); ?> <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+		<h2 class="the_title"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php echo sprintf( __('Permanent Link to %1$s','museum-core'), the_title_attribute() ); ?>"><?php the_title(); ?></a></h2>
         <div class="clear"></div>
         <div class="onecol"></div>
 		<section class="entry sixcol alt">
@@ -11,8 +11,8 @@
 		<div class="onecol last"></div>
 		<div class="clear"></div>
 		<section class="postmetadata">
-			<span class="human-time-diff alt"><?php echo human_time_diff( get_the_time('U'), current_time('timestamp') ) . __(' ago','museum-core'); ?></span><br />
-            <?php _e('Filed under ','museum-core'); the_category(',&nbsp;'); the_tags(__(' and tagged ','museum-core'),', ',''); ?><br />
+			<span class="human-time-diff alt"><?php echo sprintf(__('%1$s ago','museum-core'), human_time_diff( get_the_time('U'), current_time('timestamp') )); ?></span><br />
+            <?php echo sprintf(__('Filed under %1$s','museum-core'), the_category(',&nbsp;')); the_tags(__(' and tagged ','museum-core'),', ',''); ?><br />
             <?php comments_popup_link(__('No Comments &#187;','museum-core'), __('One Comment &#187;','museum-core'), __('% Comments &#187;','museum-core')); ?>
          </section>
 	</article>
--- post-link.php
+++ post-link.php
@@ -6,7 +6,7 @@
 			<?php wp_link_pages(); ?>
 		</section>
 		<section class="postmetadata">
-			<span class="human-time-diff alt"><?php echo human_time_diff( get_the_time('U'), current_time('timestamp') ) . __(' ago','museum-core'); ?></span>
+			<span class="human-time-diff alt"><?php echo sprintf(__('%1$s ago','museum-core'), human_time_diff( get_the_time('U'), current_time('timestamp') )); ?></span>
 		</section>
 	</article>
     <div class="clear"></div>
