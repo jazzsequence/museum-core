a91cc42e502563fd21400f572f462efa74379648
chris reynolds <chris@jazzsequence.com>
2012-04-05 08:13:02 -0600

    Revert "Revert "internationalized searchform""
    This reverts commit 382d28d4ec551b21fb784597d0208af8e47e07b0.

--- searchform.php
+++ searchform.php
@@ -1,6 +1,10 @@
+<?php
+	$searchtext = __('Search this site', 'museum-core');
+	$onfocus = "if (this.value == '$searchtext') {this.value = '';}";
+	$onblur = "if (this.value == '') {this.value = '$searchtext';}";
+?>
 <form method="get" id="searchform" action="<?php echo home_url(); ?>/">
-<div><input type="text" value="Search this site" name="s" id="s" onfocus="if (this.value == 'Search this site')
-{this.value = '';}" onblur="if (this.value == '') {this.value = 'Search this site';}" />
+<div><input type="text" value="<?php echo $searchtext; ?>" name="s" id="s" onfocus="<?php echo $onfocus; ?>" onblur="<?php echo $onblur; ?>" />
 <input type="submit" id="searchsubmit" value="Go" />
 </div>
 </form>
