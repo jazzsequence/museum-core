fa0aa67876cde083dac8341e13ee0db4130a2fc7
chris reynolds <chris@jazzsequence.com>
2012-04-05 08:04:46 -0600

    Revert "re-added 'comments are closed' just not on pages, removed the "do not delete these lines" block (that I guess can actually be removed)"
    This reverts commit d3addb11b04328c83d78057116876b65dd3ad356.

--- comments.php
+++ comments.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * @package WordPress
+ * @subpackage Default_Theme
+ */
+
+// Do not delete these lines
+	if (!empty($_SERVER['SCRIPT_FILENAME']) && 'comments.php' == basename($_SERVER['SCRIPT_FILENAME']))
+		die ('Please do not load this page directly. Thanks!');
 
 	if ( post_password_required() ) { ?>
 		<p class="nocomments"><?php _e('This post is password protected. Enter the password to view comments.','museum-core'); ?></p>
@@ -31,10 +39,7 @@
 		<!-- If comments are open, but there are no comments. -->
 
 	 <?php else : // comments are closed
-		if ( !is_page() ) {
-			// if it's not a page, display comments are closed message ?>
-			<p><?php _e('Comments are closed.','museum-core'); ?></p>
-		<?php }
+		/* If comments are closed we won't display anything (because I think displaying "Comments are closed" is ugly). */
 	 endif; ?>
 <?php endif; ?>
 
