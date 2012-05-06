833124ba43b3d69c9733f604fcbdfa6dafc163fb
chris reynolds <chris@jazzsequence.com>
2012-05-05 22:16:22 -0600

    fixes gravatar bug

--- functions.php
+++ functions.php
@@ -265,8 +265,7 @@ function ap_core_setup() {
    <li <?php comment_class(); ?> id="li-comment-<?php comment_ID() ?>">
      <div id="comment-<?php comment_ID(); ?>" class="the_comment">
       <div class="comment-author vcard">
-         <?php echo get_avatar
-	($comment,$size='64',$default='<path_to_url>' ); ?>
+         <?php echo get_avatar($comment,$size='64',$default='' ); ?>
 	<?php echo sprintf(__('On %1$s at %2$s %3$s said:','museum-core'), get_comment_date(), get_comment_time(), get_comment_author_link()) ?>
       </div>
       <?php if ($comment->comment_approved == '0') : ?>
