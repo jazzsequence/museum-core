114aeb14044d3cde86514d968fc0a2f124db7ea7
chris reynolds <chris@jazzsequence.com>
2012-05-17 15:24:33 -0600

    fixed tab spacing for comments

--- functions.php
+++ functions.php
@@ -273,27 +273,27 @@ if (!function_exists('ap_core_setup')) {
 
     	// this changes the output of the comments
     	function ap_core_comment($comment, $args, $depth) {
-       $GLOBALS['comment'] = $comment; ?>
-       <li <?php comment_class(); ?> id="li-comment-<?php comment_ID() ?>">
-         <div id="comment-<?php comment_ID(); ?>" class="the_comment">
-          <div class="comment-author vcard">
-             <?php echo get_avatar($comment,$size='64',$default='' ); ?>
-    	<?php echo sprintf(__('On %1$s at %2$s %3$s said:','museum-core'), get_comment_date(), get_comment_time(), get_comment_author_link()) ?>
-          </div>
-          <?php if ($comment->comment_approved == '0') : ?>
-             <em><?php _e('Your comment is awaiting moderation.', 'museum-core') ?></em>
-             <br />
-          <?php endif; ?>
-          <?php comment_text() ?>
-          <div class="comment-meta commentmetadata"><?php edit_comment_link(__('(Edit)', 'museum-core'),'  ','') ?></div>
-          <?php if ( comments_open() ) { ?>
-              <div class="reply"><button>
-                 <?php comment_reply_link(array_merge( $args, array('depth' => $depth, 'reply_text' => __('Respond to this','museum-core'), 'max_depth' => $args['max_depth']))) ?>
-              </button></div>
-           <?php } ?>
-         </div>
-    	<?php
-            }
+            $GLOBALS['comment'] = $comment; ?>
+            <li <?php comment_class(); ?> id="li-comment-<?php comment_ID() ?>">
+            <div id="comment-<?php comment_ID(); ?>" class="the_comment">
+                <div class="comment-author vcard">
+                    <?php echo get_avatar($comment,$size='64',$default='' ); ?>
+                    <?php echo sprintf(__('On %1$s at %2$s %3$s said:','museum-core'), get_comment_date(), get_comment_time(), get_comment_author_link()) ?>
+                </div>
+                <?php if ($comment->comment_approved == '0') : ?>
+                    <em><?php _e('Your comment is awaiting moderation.', 'museum-core') ?></em>
+                    <br />
+                <?php endif; ?>
+                <?php comment_text() ?>
+                <div class="comment-meta commentmetadata"><?php edit_comment_link(__('(Edit)', 'museum-core'),'  ','') ?></div>
+                <?php if ( comments_open() ) { ?>
+                    <div class="reply"><button>
+                    <?php comment_reply_link(array_merge( $args, array('depth' => $depth, 'reply_text' => __('Respond to this','museum-core'), 'max_depth' => $args['max_depth']))) ?>
+                    </button></div>
+                <?php } ?>
+            </div>
+            <?php
+        }
 
     	// this changes the default [...] to be a read more hyperlink
     	function ap_core_new_excerpt_more($more) {
