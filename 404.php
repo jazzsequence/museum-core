0f50a651f18906463b1bb49750343a1898448c07
Chris Reynolds <chris@jazzsequence.com>
2013-10-01 15:30:50 -0600

    bootstrap-izes the 404 template

--- 404.php
+++ 404.php
@@ -4,37 +4,29 @@
 */
 get_header();
 include( AP_CORE_OPTIONS ); ?>
-<div class="content span-9<?php echo $right; ?>">
+<div class="content col-md-9<?php echo $right; ?>">
 	<article class="post">
-	<h2 class="the_title"><?php _e('The page you were looking for could not be found.','museum-core'); ?> <span frown>:(</span></h2>
-	<p><?php _e('The page you were looking for is missing or doesn\'t exist.  Here are some links to help you back on your way.','museum-core'); ?></p>
+	<h2 class="the_title"><?php _e('Whoops! I couldn\'t find the page you were looking for.','museum-core'); ?> <span frown>:(</span></h2>
+	<p><?php _e('Try a search or one of the links below.','museum-core'); ?></p>
+	<p><?php get_search_form(); ?>
 
 	<div class="spacer-10"></div>
 
-	<nav class="span-4" id="month">
+	<nav class="col-md-6" id="month">
 		<h2><?php _e('Archives by Month','museum-core'); ?></h2>
 		<ul>
 			<?php wp_get_archives('type=monthly'); ?>
 		</ul>
 	</nav>
 
-	<nav class="span-4" id="categories">
+	<nav class="col-md-6" id="categories">
 		<h2><?php _e('Archives by Subject','museum-core'); ?></h2>
 		<ul>
 			 <?php wp_list_categories( 'title_li=' ); ?>
 		</ul>
 	</nav>
 
-	<nav class="span-4 last" id="links">
-		<h2><?php _e('Links','museum-core'); ?></h2>
-		<ul>
-			<?php wp_list_bookmarks('title_li=&categorize=0'); ?>
-		</ul>
-	</nav>
-
-	<div class="clear"></div>
 	</article>
 </div>
 <?php get_sidebar(); ?>
-<div class="clear"></div>
 <?php get_footer(); ?>
