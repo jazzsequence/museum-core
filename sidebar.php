1ad5c9e37cceb13f22d4c7137d68066203a06ddc
Chris Reynolds <chris@jazzsequence.com>
2013-10-01 15:49:43 -0600

    bootstrap-izes sidebar

--- sidebar.php
+++ sidebar.php
@@ -3,7 +3,7 @@
 	this is the sidebar
 */
 include( AP_CORE_OPTIONS ); ?>
- <div class="sidebar the_<?php echo $sidebar; ?> span-3<?php echo $last; ?>">
+ <div class="sidebar the_<?php echo $sidebar; ?> col-md-3<?php echo $last; ?>">
 	<ul>
          <!-- regular sidebar starts here -->
          <?php if ( !dynamic_sidebar(__('Sidebar','museum-core')) ) : ?>
