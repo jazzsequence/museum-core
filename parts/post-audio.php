062f20d68e3af6ffac82710064fe828e38457d4b
Chris Reynolds <chris@jazzsequence.com>
2013-10-02 15:53:01 -0600

    cleans up code (tabs)

--- parts/post-audio.php
+++ parts/post-audio.php
@@ -1,18 +1,18 @@
-	<article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
+<article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
 
-		<?php get_template_part( 'parts/part', 'title' ); ?>
+	<?php get_template_part( 'parts/part', 'title' ); ?>
 
-		<?php tha_entry_before(); ?>
-		<section class="entry">
-			<?php tha_entry_top(); ?>
+	<?php tha_entry_before(); ?>
+	<section class="entry">
+		<?php tha_entry_top(); ?>
 
-			<?php the_content(__('Read more &raquo;','museum-core')); ?>
-			<?php wp_link_pages(); ?>
+		<?php the_content(__('Read more &raquo;','museum-core')); ?>
+		<?php wp_link_pages(); ?>
 
-			<?php tha_entry_bottom(); ?>
-		</section>
-		<?php tha_entry_after(); ?>
+		<?php tha_entry_bottom(); ?>
+	</section>
+	<?php tha_entry_after(); ?>
 
-		<?php get_template_part( 'parts/part', 'postmetadata' ); ?>
+	<?php get_template_part( 'parts/part', 'postmetadata' ); ?>
 
-	</article>
+</article>
--- parts/post-chat.php
+++ parts/post-chat.php
@@ -1,20 +1,20 @@
-	<article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
+<article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
 
-		<time datetime=<?php the_time('Y-m-d'); ?>></time>
+	<time datetime=<?php the_time('Y-m-d'); ?>></time>
 
-		<?php get_template_part( 'parts/part', 'title' ); ?>
+	<?php get_template_part( 'parts/part', 'title' ); ?>
 
-		<?php tha_entry_before(); ?>
-		<section class="entry alt">
-			<?php tha_entry_top(); ?>
+	<?php tha_entry_before(); ?>
+	<section class="entry alt">
+		<?php tha_entry_top(); ?>
 
-			<?php the_content(__('Read more &raquo;','museum-core')); ?>
-			<?php wp_link_pages(); ?>
+		<?php the_content(__('Read more &raquo;','museum-core')); ?>
+		<?php wp_link_pages(); ?>
 
-			<?php tha_entry_bottom(); ?>
-		</section>
-		<?php tha_entry_after(); ?>
+		<?php tha_entry_bottom(); ?>
+	</section>
+	<?php tha_entry_after(); ?>
 
-		<?php get_template_part( 'parts/part', 'postmetadata' ); ?>
+	<?php get_template_part( 'parts/part', 'postmetadata' ); ?>
 
-	</article>
+</article>
