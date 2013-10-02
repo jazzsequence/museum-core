11335c6429036d75ffdd2c4ea6e75165135400db
Chris Reynolds <chris@jazzsequence.com>
2013-10-02 15:52:19 -0600

    cleans up code (tabs)

--- parts/part-navigation.php
+++ parts/part-navigation.php
@@ -1,6 +1,6 @@
-	<nav class="navigation clearfix">
-		<?php  if(function_exists('wp_pagenavi')) { wp_pagenavi(); } else { ?>
-		<div class="alignleft"><?php next_posts_link(__('&laquo; Older Entries','museum-core')) ?></div>
-		<div class="alignright"><?php previous_posts_link(__('Newer Entries &raquo;','museum-core')) ?></div>
-		<?php } ?>
-	</nav>
+<nav class="navigation clearfix">
+	<?php  if(function_exists('wp_pagenavi')) { wp_pagenavi(); } else { ?>
+	<div class="alignleft"><?php next_posts_link(__('&laquo; Older Entries','museum-core')) ?></div>
+	<div class="alignright"><?php previous_posts_link(__('Newer Entries &raquo;','museum-core')) ?></div>
+	<?php } ?>
+</nav>
