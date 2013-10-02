b35bf57db2370167ddf0c0b394486606a3566b00
Chris Reynolds <chris@jazzsequence.com>
2013-10-02 16:17:14 -0600

    adds tha hooks to single part

--- parts/post-single.php
+++ parts/post-single.php
@@ -1,16 +1,22 @@
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
 	<article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
+
 		<?php $is_title_set = get_the_title();
 		if ( !empty( $is_title_set ) ) { ?>
-		<h1 class="the_title"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php echo sprintf( __('Permanent Link to %s','museum-core'), the_title_attribute( 'echo=0' ) ); ?>"><?php the_title(); ?></a></h1>
+			<h1 class="the_title"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php echo sprintf( __('Permanent Link to %s','museum-core'), the_title_attribute( 'echo=0' ) ); ?>"><?php the_title(); ?></a></h1>
 		<?php } ?>
 
+		<?php tha_entry_before(); ?>
 		<section class="entry">
+			<?php tha_entry_top(); ?>
+
 			<?php the_content(__('Read more &raquo;','museum-core')); ?>
-			<div class="clear"></div>
 			<?php wp_link_pages(); ?>
+
+			<?php tha_entry_bottom(); ?>
 		</section>
+		<?php tha_entry_after(); ?>
 
 		<?php get_template_part( 'parts/part', 'postmetadata' ); ?>
 
@@ -19,9 +25,13 @@
 
         <div class="spacer-10"></div>
         <div class="spacer-10"></div>
+
+        <?php tha_comments_before(); ?>
     	<section id="comments">
 			<?php comments_template(); ?>
         </section>
+        <?php tha_comments_after(); ?>
+
 	</article>
 
 <?php endwhile; endif; ?>
