0d4f79aa493ad773796fcebbfbecd10f3bc15699
Chris Reynolds <chris@jazzsequence.com>
2013-10-02 15:16:39 -0600

    new template parts for smaller pieces of reusable code

---
+++ parts/part-micropostmeta.php
@@ -0,0 +1,5 @@
+		<section class="postmetadata">
+			<a href="<?php the_permalink(); ?>" rel="bookmark" title="<?php echo sprintf( __('Permanent Link to %s','museum-core'), the_title_attribute( 'echo=0' ) ); ?>"><?php the_title(); ?></a>
+
+			<span class="human-time-diff alt"><?php echo sprintf(__('%1$s ago','museum-core'), human_time_diff( get_the_time('U'), current_time('timestamp') )); ?></span><br />
+		</section>
