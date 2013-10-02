84911fb940a7dccc0d640a51c8a8380a1f7d7ed4
Chris Reynolds <chris@jazzsequence.com>
2013-10-02 15:31:03 -0600

    adds tha hooks to page template(s)

--- page-no-sidebar.php
+++ page-no-sidebar.php
@@ -4,9 +4,13 @@
 */
 ?>
 <?php get_header(); ?>
+<?php tha_content_before(); ?>
 <div class="content col-md-12">
+	<?php tha_content_top(); ?>
 
 	<?php get_template_part('parts/post', 'page'); ?>
 
+	<?php tha_content_bottom(); ?>
 </div>
+<?php tha_content_after(); ?>
 <?php get_footer(); ?>
