e7cc2c66e5da901d56e0ad7dc14487f1cd894d4c
Chris Reynolds <chris@jazzsequence.com>
2013-10-02 16:18:41 -0600

    status part updates
    DRYs up status
    adds tha entry hooks
    cleans up code

--- parts/post-status.php
+++ parts/post-status.php
@@ -1,11 +1,16 @@
-	<article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
-		<section class="entry">
-			<?php the_content(__('Read more &raquo;','museum-core')); ?>
-			<div class="clear"></div>
-			<?php wp_link_pages(); ?>
-		</section>
-		<section class="postmetadata">
-			<time class="human-time-diff" datetime=<?php the_time('Y-m-d'); ?>><?php echo sprintf(__('%1$s ago','museum-core'), human_time_diff( get_the_time('U'), current_time('timestamp') )); ?></time>
-         </section>
-	</article>
-    <div class="clear"></div>
+<article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
+
+	<?php tha_entry_before(); ?>
+	<section class="entry">
+		<?php tha_entry_top(); ?>
+
+		<?php the_content(__('Read more &raquo;','museum-core')); ?>
+		<?php wp_link_pages(); ?>
+
+		<?php tha_entry_bottom(); ?>
+	</section>
+	<?php tha_entry_after(); ?>
+
+	<?php get_template_part( 'parts/part', 'micropostmeta' ); ?>
+
+</article>
