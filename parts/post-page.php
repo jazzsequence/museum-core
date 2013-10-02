97db029a5c4e75624cfe43ada195a81990f4c9f7
Chris Reynolds <chris@jazzsequence.com>
2013-10-02 16:08:01 -0600

    cleans up code (tabs), adds tha hooks to page template

--- parts/post-page.php
+++ parts/post-page.php
@@ -1,20 +1,31 @@
-	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
- 		<article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
-			<time datetime=<?php the_time('Y-m-d'); ?>></time>
-			<h1 class="the_title"><?php the_title(); ?></h1>
-            <div class="clear"></div>
-			<section class="entry">
-				<?php the_content(__('Read more &raquo;','museum-core')); ?>
-				<div class="clear"></div>
-				<?php wp_link_pages(); ?>
-			</section>
-			<section class="postmetadata">
-				<p><?php edit_post_link(__('Edit this entry','museum-core'),'','.'); ?></p>
-			</section>
-    	<div class="clear"></div>
-    	<section id="comments">
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+	<article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
+
+		<time datetime=<?php the_time('Y-m-d'); ?>></time>
+		<h1 class="the_title"><?php the_title(); ?></h1>
+
+		<?php tha_entry_before(); ?>
+		<section class="entry">
+			<?php tha_entry_top(); ?>
+
+			<?php the_content(__('Read more &raquo;','museum-core')); ?>
+			<?php wp_link_pages(); ?>
+
+			<?php tha_entry_bottom(); ?>
+		</section>
+		<?php tha_entry_after(); ?>
+
+		<section class="postmetadata">
+			<p><?php edit_post_link(__('Edit this entry','museum-core'),'','.'); ?></p>
+		</section>
+
+		<?php tha_comments_before(); ?>
+		<section id="comments">
 			<?php comments_template(); ?>
         </section>
-		</article>
+        <?php tha_comments_after(); ?>
 
-	<?php endwhile; endif; ?>
+	</article>
+
+<?php endwhile; endif; ?>
