54a6e0cf9f6d560807465c00a2d3d14332a768d2
Chris Reynolds <chris@jazzsequence.com>
2013-10-02 15:15:59 -0600

    adds tha hooks to 404 template

--- 404.php
+++ 404.php
@@ -3,30 +3,36 @@
 	This is the 404 error template
 */
 get_header();
+tha_content_before();
 include( AP_CORE_OPTIONS ); ?>
 <div class="content col-md-9<?php echo $right; ?>">
+	<?php tha_content_top(); ?>
 	<article class="post">
-	<h2 class="the_title"><?php _e('Whoops! I couldn\'t find the page you were looking for.','museum-core'); ?> <span frown>:(</span></h2>
-	<p><?php _e('Try a search or one of the links below.','museum-core'); ?></p>
-	<p><?php get_search_form(); ?>
 
-	<div class="spacer-10"></div>
+		<h2 class="the_title"><?php _e('Whoops! I couldn\'t find the page you were looking for.','museum-core'); ?> <span frown>:(</span></h2>
 
-	<nav class="col-md-6" id="month">
-		<h2><?php _e('Archives by Month','museum-core'); ?></h2>
-		<ul>
-			<?php wp_get_archives('type=monthly'); ?>
-		</ul>
-	</nav>
+		<p><?php _e('Try a search or one of the links below.','museum-core'); ?></p>
+		<p><?php get_search_form(); ?>
 
-	<nav class="col-md-6" id="categories">
-		<h2><?php _e('Archives by Subject','museum-core'); ?></h2>
-		<ul>
-			 <?php wp_list_categories( 'title_li=' ); ?>
-		</ul>
-	</nav>
+		<div class="spacer-10"></div>
+
+		<nav class="col-md-6" id="month">
+			<h2><?php _e('Archives by Month','museum-core'); ?></h2>
+			<ul>
+				<?php wp_get_archives('type=monthly'); ?>
+			</ul>
+		</nav>
+
+		<nav class="col-md-6" id="categories">
+			<h2><?php _e('Archives by Subject','museum-core'); ?></h2>
+			<ul>
+				 <?php wp_list_categories( 'title_li=' ); ?>
+			</ul>
+		</nav>
 
 	</article>
+	<?php tha_content_bottom(); ?>
 </div>
+<?php tha_content_after(); ?>
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
