32543649276a3848249dec44c1b12296a3b8ea55
Chris Reynolds <chris@jazzsequence.com>
2013-10-02 15:32:52 -0600

    search.php updates
    DRYs up search template part
    adds bootstrap media classes
    cleans up the code
    adds tha content hooks to search template

--- parts/post-search.php
+++ parts/post-search.php
@@ -13,7 +13,8 @@
     global $wp_query;
     $total_results = $wp_query->found_posts;
 ?>
-	<h1 class="searchresults the_title"><?php echo sprintf( __( 'We found %1$s results for &ldquo;%2$s&rdquo;', 'museum-core' ), $total_results, get_search_query() ); ?></h1>
+
+	<h1 class="searchresults the_title"><?php echo sprintf( __( 'We found %1$s results for <q>%2$s</q>', 'museum-core' ), $total_results, get_search_query() ); ?></h1>
 
 	<section class="searchform">
 		<h3 class="alt"><?php _e( 'Not what you were looking for?  Enter your search terms to try again.', 'museum-core' ); ?></h3>
@@ -22,47 +23,24 @@
 
 	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 	<article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
-		<h3 class="the_date"><time datetime=<?php the_time('Y-m-d'); ?>><?php the_time(get_option('date_format')) ?></time></h3>
-		<?php $is_title_set = get_the_title();
-		if ( empty( $is_title_set ) ) { ?>
-			<h1 class="the_title"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php echo sprintf( __('Permanent Link to %s','museum-core'), the_title_attribute( 'echo=0' ) ); ?>"><?php _e('(no title)', 'museum-core'); ?></a></h1>
-		<?php } ?>
-		<h1 class="the_title"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php echo sprintf( __('Permanent Link to %s','museum-core'), the_title_attribute( 'echo=0' ) ); ?>"><?php the_title(); ?></a></h1>
-        <div class="clear"></div>
-		<section class="entry">
+
+		<?php get_template_part( 'parts/part', 'title' ); ?>
+
+		<section class="entry media">
 			<?php if(has_post_thumbnail()) { ?>
-				<div class="alignleft span-2"><a href="<?php the_permalink() ?>" title="<?php the_title_attribute(); ?>"><?php the_post_thumbnail(); ?></a></div>
+				<div class="pull-left"><a href="<?php the_permalink() ?>" title="<?php the_title_attribute(); ?>"><?php the_post_thumbnail( 'thumbnail', array( 'class' => 'img-thumbnail media-object' ) ); ?></a></div>
 			<?php } ?>
-			<?php the_excerpt(); ?>
+			<div class="media-body">
+				<?php the_excerpt(); ?>
+			</div>
 		</section>
-		<section class="postmetadata">
-			<?php
-				$options = get_option( 'ap_core_theme_options' );
-            	$categories = get_the_category_list( __(', ', 'museum-core') );
-				$tags = get_the_tag_list( __('and tagged ', 'museum-core'),', ' );
-				$author_name = get_the_author_meta('display_name');
-				$author_ID = get_the_author_meta('ID');
-				$author_link = '<a href="' . get_author_posts_url($author_ID) . '">' . $author_name . '</a>';
-				$author = 'by ' . $author_link;
-				if ( $options['post-author'] ) {
-					$postmeta = __('Posted in %1$s %2$s %3$s', 'museum-core');
-				} else {
-					$postmeta = __('Posted in %1$s %2$s', 'museum-core');
-				}
-				printf( $postmeta, $categories, $tags, $author );
-			?>
-			<br />
-            <?php comments_popup_link(__('No Comments &#187;','museum-core'), __('One Comment &#187;','museum-core'), __('% Comments &#187;','museum-core')); ?>
-        </section>
+
+		<?php get_template_part( 'parts/part', 'postmetadata' ); ?>
+
 	</article>
-    <div class="clear"></div>
 
 	<?php endwhile; ?>
 
-	<nav class="navigation">
-		<?php  if(function_exists('wp_pagenavi')) { wp_pagenavi(); } else { ?>
-		<div class="alignleft"><?php next_posts_link(__('&laquo; Older Entries','museum-core')) ?></div>
-		<div class="alignright"><?php previous_posts_link(__('Newer Entries &raquo;','museum-core')) ?></div>
-		<?php } ?>
-	</nav>
+	<?php get_template_part( 'parts/part', 'navigation' ); ?>
+
 	<?php endif; ?>
