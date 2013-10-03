4ede9a25a72013416f7684e3aa77c5a88e853aaa
Chris Reynolds <chris@jazzsequence.com>
2013-10-03 14:19:38 -0600

    adds better formatting for chat posts

--- functions.php
+++ functions.php
@@ -386,6 +386,145 @@ function ap_core_link_pages( $args = array () ) {
     print $output . $after;
 }
 
+/* Filter the content of chat posts. */
+add_filter( 'the_content', 'ap_core_chat_content' );
+
+/* Auto-add paragraphs to the chat text. */
+add_filter( 'ap_core_chat_text', 'wpautop' );
+
+/**
+ * This function filters the post content when viewing a post with the "chat" post format.  It formats the
+ * content with structured HTML markup to make it easy for theme developers to style chat posts.  The
+ * advantage of this solution is that it allows for more than two speakers (like most solutions).  You can
+ * have 100s of speakers in your chat post, each with their own, unique classes for styling.
+ *
+ * @author David Chandra
+ * @link http://www.turtlepod.org
+ * @author Justin Tadlock
+ * @link http://justintadlock.com
+ * @copyright Copyright (c) 2012
+ * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * @link http://justintadlock.com/archives/2012/08/21/post-formats-chat
+ *
+ * @global array $_post_format_chat_ids An array of IDs for the chat rows based on the author.
+ * @param string $content The content of the post.
+ * @return string $chat_output The formatted content of the post.
+ */
+function ap_core_chat_content( $content ) {
+    global $_post_format_chat_ids;
+
+    /* If this is not a 'chat' post, return the content. */
+    if ( !has_post_format( 'chat' ) )
+        return $content;
+
+    /* Set the global variable of speaker IDs to a new, empty array for this chat. */
+    $_post_format_chat_ids = array();
+
+    /* Allow the separator (separator for speaker/text) to be filtered. */
+    $separator = apply_filters( 'ap_core_chat_separator', ':' );
+
+    /* Open the chat transcript div and give it a unique ID based on the post ID. */
+    $chat_output = "\n\t\t\t" . '<ul id="chat-transcript-' . esc_attr( get_the_ID() ) . '" class="chat-transcript list-group">';
+
+    /* Split the content to get individual chat rows. */
+    $chat_rows = preg_split( "/(\r?\n)+|(<br\s*\/?>\s*)+/", $content );
+
+    /* Loop through each row and format the output. */
+    foreach ( $chat_rows as $chat_row ) {
+
+        /* If a speaker is found, create a new chat row with speaker and text. */
+        if ( strpos( $chat_row, $separator ) ) {
+
+            /* Split the chat row into author/text. */
+            $chat_row_split = explode( $separator, trim( $chat_row ), 2 );
+
+            /* Get the chat author and strip tags. */
+            $chat_author = strip_tags( trim( $chat_row_split[0] ) );
+
+            /* Get the chat text. */
+            $chat_text = trim( $chat_row_split[1] );
+
+            /* Get the chat row ID (based on chat author) to give a specific class to each row for styling. */
+            $speaker_id = ap_core_chat_row_id( $chat_author );
+
+            /* Open the chat row. */
+            $chat_output .= "\n\t\t\t\t" . '<li class="list-group-item chat-row ' . sanitize_html_class( "chat-speaker-{$speaker_id}" ) . '">';
+
+            /* Add the chat row author. */
+            $chat_output .= "\n\t\t\t\t\t" . '<div class="pull-left chat-author ' . sanitize_html_class( strtolower( "chat-author-{$chat_author}" ) ) . ' vcard"><cite class="fn">' . apply_filters( 'ap_core_chat_author', $chat_author, $speaker_id ) . '</cite>' . $separator . '</div>';
+
+            /* Add the chat row text. */
+            $chat_output .= "\n\t\t\t\t\t" . '<div class="chat-text">' . str_replace( array( "\r", "\n", "\t" ), '', apply_filters( 'ap_core_chat_text', $chat_text, $chat_author, $speaker_id ) ) . '</div>';
+
+            /* Close the chat row. */
+            $chat_output .= "\n\t\t\t\t" . '</li><!-- .chat-row -->';
+        }
+
+        /**
+         * If no author is found, assume this is a separate paragraph of text that belongs to the
+         * previous speaker and label it as such, but let's still create a new row.
+         */
+        else {
+
+            /* Make sure we have text. */
+            if ( !empty( $chat_row ) ) {
+
+                /* Open the chat row. */
+                $chat_output .= "\n\t\t\t\t" . '<li class="list-group-item chat-row ' . sanitize_html_class( "chat-speaker-{$speaker_id}" ) . '">';
+
+                /* Don't add a chat row author.  The label for the previous row should suffice. */
+
+                /* Add the chat row text. */
+                $chat_output .= "\n\t\t\t\t\t" . '<div class="chat-text">' . str_replace( array( "\r", "\n", "\t" ), '', apply_filters( 'ap_core_chat_text', $chat_row, $chat_author, $speaker_id ) ) . '</div>';
+
+                /* Close the chat row. */
+                $chat_output .= "\n\t\t\t</li><!-- .chat-row -->";
+            }
+        }
+    }
+
+    /* Close the chat transcript div. */
+    $chat_output .= "\n\t\t\t</ul><!-- .chat-transcript -->\n";
+
+    /* Return the chat content and apply filters for developers. */
+    return apply_filters( 'ap_core_chat_content', $chat_output );
+}
+
+/**
+ * This function returns an ID based on the provided chat author name.  It keeps these IDs in a global
+ * array and makes sure we have a unique set of IDs.  The purpose of this function is to provide an "ID"
+ * that will be used in an HTML class for individual chat rows so they can be styled.  So, speaker "John"
+ * will always have the same class each time he speaks.  And, speaker "Mary" will have a different class
+ * from "John" but will have the same class each time she speaks.
+ *
+ * @author David Chandra
+ * @link http://www.turtlepod.org
+ * @author Justin Tadlock
+ * @link http://justintadlock.com
+ * @copyright Copyright (c) 2012
+ * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * @link http://justintadlock.com/archives/2012/08/21/post-formats-chat
+ *
+ * @global array $_post_format_chat_ids An array of IDs for the chat rows based on the author.
+ * @param string $chat_author Author of the current chat row.
+ * @return int The ID for the chat row based on the author.
+ */
+function ap_core_chat_row_id( $chat_author ) {
+    global $_post_format_chat_ids;
+
+    /* Let's sanitize the chat author to avoid craziness and differences like "John" and "john". */
+    $chat_author = strtolower( strip_tags( $chat_author ) );
+
+    /* Add the chat author to the array. */
+    $_post_format_chat_ids[] = $chat_author;
+
+    /* Make sure the array only holds unique values. */
+    $_post_format_chat_ids = array_unique( $_post_format_chat_ids );
+
+    /* Return the array key for the chat author and add "1" to avoid an ID of "0". */
+    return absint( array_search( $chat_author, $_post_format_chat_ids ) ) + 1;
+}
+
 /**
  * Meta generator
  * @since 0.4.5
