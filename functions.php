0aa64560d4bb28405719bb3835d1eb5dab59834e
Chris Reynolds <chris@jazzsequence.com>
2013-10-03 12:02:12 -0600

    adds new funtion which replaces wp_list_pages and supports twitter bootstrap pagination

--- functions.php
+++ functions.php
@@ -333,6 +333,60 @@ if (!function_exists('ap_core_wp_title')) {
 }
 
 /**
+ * Link Pages
+ * @author toscha
+ * @since 2.0.0
+ * @link http://wordpress.stackexchange.com/questions/14406/how-to-style-current-page-number-wp-link-pages
+ * @param  array $args
+ * @return void
+ * Modification of wp_link_pages() with an extra element to highlight the current page.
+ */
+function ap_core_link_pages( $args = array () ) {
+    $defaults = array(
+        'before'      => '<p>' . __('Pages:'),
+        'after'       => '</p>',
+        'before_link' => '',
+        'after_link'  => '',
+        'current_before' => '',
+        'current_after' => '',
+        'link_before' => '',
+        'link_after'  => '',
+        'pagelink'    => '%',
+        'echo'        => 1
+    );
+
+    $r = wp_parse_args( $args, $defaults );
+    $r = apply_filters( 'wp_link_pages_args', $r );
+    extract( $r, EXTR_SKIP );
+
+    global $page, $numpages, $multipage, $more, $pagenow;
+
+    if ( ! $multipage )
+    {
+        return;
+    }
+
+    $output = $before;
+
+    for ( $i = 1; $i < ( $numpages + 1 ); $i++ )
+    {
+        $j       = str_replace( '%', $i, $pagelink );
+        $output .= ' ';
+
+        if ( $i != $page || ( ! $more && 1 == $page ) )
+        {
+            $output .= "{$before_link}" . _wp_link_page( $i ) . "{$link_before}{$j}{$link_after}</a>{$after_link}";
+        }
+        else
+        {
+            $output .= "{$current_before}{$link_before}<a>{$j}</a>{$link_after}{$current_after}";
+        }
+    }
+
+    print $output . $after;
+}
+
+/**
  * Meta generator
  * @since 0.4.5
  * @author Chris Reynolds
