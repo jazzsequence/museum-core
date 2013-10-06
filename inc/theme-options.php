41b897ca646521c03cf8cf5fc093d66b8c07a652
Chris Reynolds <chris@jazzsequence.com>
2013-10-06 09:30:11 -0600

    checks if custom css is set, but the value is empty

--- inc/theme-options.php
+++ inc/theme-options.php
@@ -51,7 +51,7 @@ if ( !function_exists( 'ap_core_theme_customizer_init' ) ) {
 
 			$options = get_option( 'ap_core_theme_options' );
 
-			if ( isset( $options['css'] ) && ( $options['css'] != '1' ) ) {
+			if ( isset( $options['css'] ) && ( $options['css'] != '1' ) && ( $options['css'] != '' ) ) {
 				echo '<div style="background-color: #fcf8e3; border: 1px solid #fbeed5; border-radius: 4px; padding: 2px 7px;"><label>';
 				echo '<span class="customize-control-title">' . __( 'Custom CSS is no longer supported.', 'museum-core' ) . '</span>';
 				echo sprintf( _x( 'Museum Core no longer supports custom CSS. Please use %1$sMy Custom CSS%2$s or %3$sJetpack%2$s to add custom CSS to your site. Your Custom CSS is displayed below.', '1: link to My Custom CSS, 2: closing <a> tag, 3: link to Jetpack', 'museum-core' ), '<a href="wordpress.org/plugins/my-custom-css/" target="_blank">', '</a>', '<a href="http://wordpress.org/plugins/jetpack" target="_blank">' );
