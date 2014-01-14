832ca3af6ac4bfcd0a5b4173462d8caa847449f6
Chris Reynolds <chris@jazzsequence.com>
2014-01-13 17:55:43 -0700

    removes conditional html classes
    closes #50

--- header.php
+++ header.php
@@ -1,11 +1,6 @@
 <!doctype html>
 <?php tha_html_before(); ?>
-<!-- paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither/ -->
-<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" <?php language_attributes(); ?>> <![endif]-->
-<!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8" <?php language_attributes(); ?>> <![endif]-->
-<!--[if IE 8]>    <html class="no-js lt-ie9" <?php language_attributes(); ?>> <![endif]-->
-<!-- Consider adding a manifest.appcache: h5bp.com/d/Offline -->
-<!--[if gt IE 8]><!--> <html <?php language_attributes(); ?>> <!--<![endif]-->
+<html <?php language_attributes(); ?>>
 <head>
 <?php tha_head_top(); ?>
 <meta charset="<?php bloginfo('charset'); ?>">
