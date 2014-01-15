6b8a2d6af65c9ca44e7756d4f12a71817f069268
jazzsequence <chris@jazzsequence.com>
2014-01-15 15:31:56 -0700

    adds date tag to quote and status posts which don't display the date otherwise
    this also makes them more uniform with other template parts that don't display the date like asides and links.

--- parts/post-quote.php
+++ parts/post-quote.php
@@ -1,16 +1,18 @@
-	<article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
+<article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
 
-		<?php tha_entry_before(); ?>
-		<section class="entry">
-			<?php tha_entry_top(); ?>
+	<time datetime=<?php the_time('Y-m-d'); ?>></time>
 
-			<?php the_content(__('Read more &raquo;','museum-core')); ?>
-			<?php get_template_part( 'parts/part', 'link-pages' ); ?>
+	<?php tha_entry_before(); ?>
+	<section class="entry">
+		<?php tha_entry_top(); ?>
 
-			<?php tha_entry_bottom(); ?>
-		</section>
-		<?php tha_entry_after(); ?>
+		<?php the_content(__('Read more &raquo;','museum-core')); ?>
+		<?php get_template_part( 'parts/part', 'link-pages' ); ?>
 
-		<div class="icon icon-quote-left pull-left" title="<?php _e( 'Quote', 'museum-core' ); ?>"></div><?php get_template_part( 'parts/part', 'micropostmeta' ); ?>
+		<?php tha_entry_bottom(); ?>
+	</section>
+	<?php tha_entry_after(); ?>
 
-	</article>
+	<div class="icon icon-quote-left pull-left" title="<?php _e( 'Quote', 'museum-core' ); ?>"></div><?php get_template_part( 'parts/part', 'micropostmeta' ); ?>
+
+</article>
