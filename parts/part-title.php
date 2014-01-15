d968903621bfa3b07f03745297c025c6471c3d8d
jazzsequence <chris@jazzsequence.com>
2014-01-15 16:23:02 -0700

    changed the way the date displays
    made it an h2 with a Bootstrap <small> tag. more spacing and looks way nicer.

--- parts/part-title.php
+++ parts/part-title.php
@@ -1,11 +1,10 @@
 <?php
-$format = get_post_format();
-$no_date_formats = array( 'chat', 'gallery', 'image' ); ?>
-<?php if ( !in_array( $format, $no_date_formats ) ) { ?>
-    <h3 class="the_date"><time datetime=<?php the_time('Y-m-d'); ?>><?php the_time(get_option('date_format')) ?></time></h3>
-<?php } ?>
+$format = get_post_format(); ?>
 
-<?php $is_title_set = get_the_title();
+<h2 class="the_date alt"><small><time datetime=<?php the_time('Y-m-d'); ?>><?php the_time(get_option('date_format')) ?></time></small></h2>
+
+<?php
+$is_title_set = get_the_title();
 if ( empty( $is_title_set ) ) { ?>
 	<h1 class="the_title"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php echo sprintf( __('Permanent Link to %s','museum-core'), the_title_attribute( 'echo=0' ) ); ?>"><?php _e('(no title)', 'museum-core'); ?></a></h1>
 <?php } else { ?>
