1b21f00ee0f10ce57dcae296ea2a1915bfd548b7
jazzsequence <chris@jazzsequence.com>
2014-01-16 12:26:12 -0700

    no real need for this here

--- searchform.php
+++ searchform.php
@@ -6,7 +6,7 @@
 		<label class="sr-only" for="search">
 			<?php echo $ap_core_searchtext; ?>
 		</label>
-		<input type="search" class="form-control col-md-12" placeholder="<?php echo $ap_core_searchtext; ?>" name="s" id="s" />
+		<input type="search" class="form-control" placeholder="<?php echo $ap_core_searchtext; ?>" name="s" id="s" />
 		<button type="submit" id="searchsubmit" class="btn btn-default"><?php esc_attr_e('Go', 'museum-core'); ?></button>
 	</div>
 </form>
