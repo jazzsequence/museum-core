fe9e331dce704f53d4f7fa4670084bd02ec6f0cd
jazzsequence <chris@jazzsequence.com>
2014-01-16 15:34:41 -0700

    removes a lot of cruft from the comments

--- comments.php
+++ comments.php
@@ -1,15 +1,13 @@
 <?php
 
-	if ( post_password_required() ) { ?>
-		<p class="nocomments"><?php _e('This post is password protected. Enter the password to view comments.','museum-core'); ?></p>
-	<?php
-		return;
-	}
-?>
+if ( post_password_required() ) { ?>
 
-<!-- You can start editing here. -->
+	<p class="nocomments"><?php _e('This post is password protected. Enter the password to view comments.','museum-core'); ?></p>
+	<?php return;
 
-<?php if ( have_comments() ) : ?>
+}
+
+if ( have_comments() ) : ?>
 	<h3 id="comments"><?php printf( _n( 'One Response to %2$s', '%1$s Responses to %2$s', get_comments_number(), 'museum-core' ), number_format_i18n( get_comments_number() ), '<span>' . get_the_title() . '</span>' ); ?></h3>
 
 	<nav class="navigation clearfix">
@@ -29,32 +27,20 @@
 			<li class="next"><?php next_comments_link() ?></li>
 		</ul>
 	</nav>
- <?php else : // this is displayed if there are no comments so far ?>
+ <?php endif; // ends have_comments()
 
-	<?php if ( comments_open() ) : ?>
-		<!-- If comments are open, but there are no comments. -->
+if ( !comments_open() && '0' != get_comments_number() && post_type_supports( get_post_type(), 'comments' ) && !is_page() ) :
 
-	 <?php else : // comments are closed
-		if ( !is_page() ) {
-			// if it's not a page, display comments are closed message ?>
-			<p><?php _e('Comments are closed.','museum-core'); ?></p>
-		<?php }
-	 endif; ?>
-<?php endif; ?>
+		// if it's not a page, display comments are closed message ?>
+		<p><?php _e('Comments are closed.','museum-core'); ?></p>
 
+<?php endif;
 
-<?php if ( comments_open() ) : ?>
+if ( comments_open() ) : ?>
 
-<div class="cancel-comment-reply">
-	<small><?php cancel_comment_reply_link(); ?></small>
-</div>
-
-<?php if ( get_option('comment_registration') && !is_user_logged_in() ) : ?>
-<p><?php
-	$ap_anchor = '<a href="' . wp_login_url( get_permalink() ) . '">';
-	$ap_anchorclose = '</a>';
-	echo sprintf( __('You must be %1$slogged in%$2s to post a comment.','museum-core'), $ap_anchor, $ap_anchorclose ); ?></p>
-<?php else : ?>
+	<div class="cancel-comment-reply">
+		<small><?php cancel_comment_reply_link( '<span class="text-danger">' . __( 'Cancel comment response', 'museum-core' ) . '</span>' ); ?></small>
+	</div>
 
 <?php
 
@@ -64,10 +50,6 @@
 
 	$ap_comment_notes_after = '<div class="form-group form-allowed-tags">' . sprintf( __( 'You may use these <abbr title="HyperText Markup Language">HTML</abbr> tags and attributes: %s', 'museum-core' ), ' <pre>' . allowed_tags() . '</pre>' ) . '</div>';
 
-?>
-
-<?php comment_form( array( 'comment_field' => $ap_comment_form, 'comment_notes_after' => $ap_comment_notes_after ) ); ?>
-
-<?php endif; // If registration required and not logged in ?>
+	comment_form( array( 'comment_field' => $ap_comment_form, 'comment_notes_after' => $ap_comment_notes_after ) ); ?>
 
 <?php endif; // if you delete this the sky will fall on your head ?>
