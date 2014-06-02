f1a97b45121a69b7e98cf677eff1f7a06c17c8c0
Chris Reynolds <chris@jazzsequence.com>
2014-06-02 14:07:11 -0600

    removes presstrends support
    I don't know what's up with them these days but it appears to be dead.

--- inc/theme-options.php
+++ inc/theme-options.php
@@ -300,15 +300,6 @@ if ( !function_exists( 'ap_core_theme_customizer_init' ) ) {
 
 		) );
 
-		$wp_customize->add_setting( 'ap_core_theme_options[presstrends]', array(
-
-			'default' => $defaults['presstrends'],
-			'capability' => 'edit_theme_options',
-			'transport' => 'refresh',
-			'type' => 'option'
-
-		) );
-
 		$wp_customize->add_setting( 'ap_core_theme_options[generator]', array(
 
 			'default' => $defaults['generator'],
@@ -569,17 +560,6 @@ if ( !function_exists( 'ap_core_theme_customizer_init' ) ) {
 		) );
 		$wp_customize->add_control( $favicon );
 
-		$wp_customize->add_control( 'ap_core_theme_options[presstrends]', array(
-
-			'label' => __( 'Send usage data?', 'museum-core' ),
-			'section' => 'ap_core_advanced',
-			'settings' => 'ap_core_theme_options[presstrends]',
-			'type' => 'select',
-			'choices' => ap_core_true_false(),
-			'sanitize_callback' => 'ap_core_validate_true_false'
-
-		) );
-
 		$wp_customize->add_control( 'ap_core_theme_options[generator]', array(
 
 			'label' => __( 'Debug Mode Active', 'museum-core' ),
@@ -670,59 +650,6 @@ if ( !function_exists( 'ap_core_customize_preview' ) ) {
 		<?php
 	}
 
-}
-
-// Start of PressTrends Magic
-if (!function_exists('ap_core_presstrends')) {
-	function ap_core_presstrends() {
-
-	// PressTrends Account API Key
-	$api_key = 'i93727o4eba1lujhti5bjgiwfmln5xm5o0iv';
-	$plugin_name = ''; // sets the plugin_name varible with something to fix that not defined error...
-
-	// Start of Metrics
-	global $wpdb;
-	$data = get_transient( 'presstrends_data' );
-	if (!$data || $data == ''){
-	$api_base = 'http://api.presstrends.io/index.php/api/sites/update/api/';
-	$url = $api_base . $api_key . '/';
-	$data = array();
-	$count_posts = wp_count_posts();
-	$count_pages = wp_count_posts('page');
-	$comments_count = wp_count_comments();
-	$theme_data = get_theme_data(get_stylesheet_directory() . '/style.css');
-	$plugin_count = count(get_option('active_plugins'));
-	$all_plugins = get_plugins();
-	foreach($all_plugins as $plugin_file => $plugin_data) {
-	$plugin_name .= $plugin_data['Name'];
-	$plugin_name .= '&';}
-	$posts_with_comments = $wpdb->get_var("SELECT COUNT(*) FROM {$wpdb->prefix}posts WHERE post_type='post' AND comment_count > 0");
-	$comments_to_posts = number_format(($posts_with_comments / $count_posts->publish) * 100, 0, '.', '');
-	$pingback_result = $wpdb->get_var('SELECT COUNT(comment_ID) FROM '.$wpdb->comments.' WHERE comment_type = "pingback"');
-	$data['url'] = stripslashes(str_replace(array('http://', '/', ':' ), '', site_url()));
-	$data['posts'] = $count_posts->publish;
-	$data['pages'] = $count_pages->publish;
-	$data['comments'] = $comments_count->total_comments;
-	$data['approved'] = $comments_count->approved;
-	$data['spam'] = $comments_count->spam;
-	$data['pingbacks'] = $pingback_result;
-	$data['post_conversion'] = $comments_to_posts;
-	$data['theme_version'] = $theme_data['Version'];
-	$data['theme_name'] = $theme_data['Name'];
-	$data['site_name'] = str_replace( ' ', '', get_bloginfo( 'name' ));
-	$data['plugins'] = $plugin_count;
-	$data['plugin'] = urlencode($plugin_name);
-	$data['wpversion'] = get_bloginfo('version');
-	foreach ( $data as $k => $v ) {
-	$url .= $k . '/' . $v . '/';}
-	$response = wp_remote_get( $url );
-	set_transient('presstrends_data', $data, 60*60*24);}
-	}
-
-	$options = get_option( 'ap_core_theme_options' );
-	if ( $options['presstrends'] == true ) {
-	add_action('admin_init', 'ap_core_presstrends');
-	}
 }
 
 /**
